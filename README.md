a5b2aecd1c0bd14a37a6e74f6e580205cba89039
Uğur Arıcı <m.ugurarici@gmail.com>
2016-11-16 04:11:40 +0200

    v1.2 Güncellemesi - Artık yayın için gerekli tüm çıktılar /public dizininde bulunacak

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Her şeyden önce bilgisayarınızda nodejs ve haliyle npm kurulu olmalıdır. T
 
 Dosyaları ilk kopyalamanızın ardından, terminalden ilgili dizinin içine gelip sırasıyla ```npm install``` ve tamamlandıktan sonra ```bower install``` komutlarını çalıştırmalısınız.
 
-Bunların tamamlanmasının ardından aynı dizin içerisindeyken ```gulp``` komutunu çalıştırarak geliştirme dizini içindeki dosyaların, gulp tarafından işlenip, /assets dizini içine css ve js dosyaları, ana dizine de html dosyaları gelecek şekilde kopyalanmasını sağlayabilirsiniz. Bu komutla birlikte, terminali kapatmadığınız sürece, /dev geliştirme dizinindeki değişiklikleir takip eder hale gelirsiniz. Herhangi bir değişiklik uyguladığınızda gulp, gereken görevleri çalıştırır ve ilgili dosyaları günceller.
+Bunların tamamlanmasının ardından aynı dizin içerisindeyken ```gulp``` komutunu çalıştırarak geliştirme dizini içindeki dosyaların, gulp tarafından işlenip, yayına atılacak tüm dosyaların bulunacağı ```/public``` dizini içerisine kaydedilir. ```/public/assets``` dizini içine css ve js dosyaları, direkt ```/public``` dizinine de html dosyaları gelecek şekilde kopyalanacaktır. Bu komutla birlikte, terminali kapatmadığınız sürece, ```/dev``` geliştirme dizinindeki değişiklikleir takip eder hale gelirsiniz. Herhangi bir değişiklik uyguladığınızda gulp, gereken görevleri çalıştırır ve ilgili dosyaları günceller.
 
 ## Ne nerede?
 Geliştirme yaparken kullanacağımız temel yapı ```/dev``` dizini içerisinde. Daha detaylı bir açıklama yapılabilir ancak içindeki klasörler ve örnek dosyalar açıklayıcı olacaktır.
@@ -33,10 +33,26 @@ Geliştirme yaparken kullanacağımız temel yapı ```/dev``` dizini içerisinde
 ```/dev/js```içerisinde javascript, ```/dev/scss``` içerisinde css dosyaları için geliştirme yapısını inceleyebilirsiniz. Bu alandaki hangi dosyanın, hangi gulp göreviyle nasıl işlenip nereye kaydedildiğini ```gulpfile.js```dosyasını inceleyerek anlayabilirsiniz. Ancak yapının kendi halinde zaten ilgili dosyaların yolları, html içine çekilerek kullanıma hazır hale getirilmektedir.
 
 ### HTML Dosyaları
-```/dev/layouts```dizini içerisinde, HTML sayfalarda tekrar kullanmamız gerekebilecek parçaları oluştururken (header, footer, menu, sidebar vs), ```/dev/pages``` dizininde ise, gulp görevleri ile işlenip ana dizine asıl ürün olarak kaydedilecek HTML sayfalarını oluştururuz. İlgili dizinler de üzerinden ilerleyebileceğiniz örnek barındırmaktadır.
+```/dev/layouts```dizini içerisinde, HTML sayfalarda tekrar kullanmamız gerekebilecek parçaları oluştururken (header, footer, menu, sidebar vs), ```/dev/pages``` dizininde ise, gulp görevleri ile işlenip ```/public``` dizinine asıl ürün olarak kaydedilecek HTML sayfalarını oluştururuz. İlgili dizinler de üzerinden ilerleyebileceğiniz örnek barındırmaktadır.
 
 ### /dev/vendor nedir?
-```/dev/vendor```dizini içerisinde bower tarafından dahil edilen paketlerin tutulması sağlanır. .gitignore kaydı ile git tarafından görmezden gelindiği unutulmamalıdır.
+```/dev/vendor```dizini içerisinde bower tarafından dahil edilen paketlerin tutulması sağlanır. bower bir paket yöneticisidir ve projenize dışarıdan front-end paketleri dahil edebilmenizi sağlar. .gitignore kaydı ile git tarafından görmezden gelindiği unutulmamalıdır.
+
+### Dışarıdan nasıl paket dahil edebilirim?
+**bower** paketleri ```/dev/vendor``` dizini altında, **npm** paketleri ise ```/node_modules``` dizini altında bulunacaktır. Bu paket yöneticileri ile sistemine dahil etmek istediğiniz paketlerin isim ve sürüm bilgilerini bower için ```bower.json``` ya da npm için ```package.json``` dosyasında belirtmelisiniz. Bu işlemler için terminal üzerinden komut da girebilirsiniz. Eklediğiniz paketlerin iligli paket yöneticisinin .json dosyasına da eklendiğini ya da .json dosyalarını elle güncelledikten sonra ilgili paket yöneticisinin güncelleme (update) komutunu çalıştırmayı unutmayın.
+
+[bower](https://bower.io) ve [npm](https://www.npmjs.com) hakkında detaylı bilgi için resmi web sitelerini ziyaret edin.
+
+https://bower.io
+https://www.npmjs.com
+
+### Ekleyeceğim paket ya da dosyalar bower ya da npm'de bulunmuyorsa?
+bower ve npm paketleri ile proje dizinimize kopyalanan dosyaların git depomuza gönderilmediğini unutmayın. Bu yüzden dışarıdan elle ekleyeceğimiz paket ya da dosyaları bu klasörlerin içine değil, gerekirse doğrudan ```/public/assets/``` içindeki ilgili alana kopyalanmasını sağlayın.
+
+---
+
+## Yayına nasıl çıkılır?
+Geliştirme dosyaları, paketler, bağımlılıklar vs hepsinin toplanıp yayın için hazırlanıp kaydedildiği yer ```/public``` dizinidir. Bu dizin içindeki dosyaları web projenizin bulunacağı sunucunuza kopyalamanız durumunda yayına hazır hale gelmiş olursunuz. Geri kalan her şey sadece geliştiricilerin görmesi gereken, geliştirme alanıdır.
 
 ---
 
@@ -53,6 +69,11 @@ Lütfen katkıda bulunmaktan çekinmeyin ve bu iş için ortak dil olabilecek bi
 ---
 
 ## Güncelleme Geçmişi
+### v1.2
+* Yayına gidecek her şey tek dizin altında toplanır,
+* Geliştirme sonucu oluşan ve yayına gönderilecek her şey ```/public```dizini içerisine kaydedilir. Bunu biz değil, görevleri otomatikleştiren gulp sağlar.
+* Artık yayına çıkacağımız zaman direkt ```/public```dizini içeriğini sunucumuza göndermemiz yeterlidir.
+
 ### v1.1
 * Yeni gulp eklentileri ile gulp görevleri güncellendi
 * HTML dosyalarının tek satıra sıkıştırılması için _gulp-htmlmin_ dahil edildi
--- gulpfile.js
+++ gulpfile.js
@@ -1,233 +1,235 @@
 /* 
 
-    Gulp eklenti tanimlamalari 
-    
-    @sass 
-    sass dosyalarini css'e cevirir.
-    
-    @uglifiy_js
-    js dosyalarini minify eder.
-    
-    @file_include
-    html leri ayristirip moduler bir yapi olusturur.
-    
-    @concat_css
-    css dosyalarını birlestirir.
-    
-    @concat 
-    js dosyalarini birlestirir.
-    
-    @minify_css
-    css dosyalarini minify eder.
-    
-    @rename
-    dosyalari yeniden isimlendirebilmemize olanak saglar.  
+Gulp eklenti tanimlamalari 
 
-    @htmlmin
-    html dosyalarını minify etmeye yarar, js kodundaki kırılımlar olmadığı sürece tek satıra indirir
-    
- 
-    */
+@sass 
+sass dosyalarini css'e cevirir.
 
-    var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    uglify_js = require("gulp-uglifyjs"),
-    minify = require("gulp-minify"),
-    file_include = require("gulp-file-include"),
-    concat_css = require("gulp-concat-css"),
-    concat = require("gulp-concat"),
-    minify_css = require("gulp-minify-css"),
-    rename = require("gulp-rename"),
-    htmlmin = require('gulp-htmlmin');
-    
+@uglifiy_js
+js dosyalarini minify eder.
+
+@file_include
+html leri ayristirip moduler bir yapi olusturur.
+
+@concat_css
+css dosyalarını birlestirir.
+
+@concat 
+js dosyalarini birlestirir.
+
+@minify_css
+css dosyalarini minify eder.
+
+@rename
+dosyalari yeniden isimlendirebilmemize olanak saglar.  
+
+@htmlmin
+html dosyalarını minify etmeye yarar, js kodundaki kırılımlar olmadığı sürece tek satıra indirir
 
 
+*/
 
-/* 
-
-    Assets (assets) klasorunde urun ortamina hazir sekilde yapilandirilmis klasorler ve dosyalar bulunur.
-    Dev (dev) klasorunde projenin yapilandirilandigi klasorler ve dosyalar bulunur.
-    
-    */
-
-    var assets_dir = "assets/",
-    assets_css_dir = "assets/css/",
-    assets_js_dir = "assets/js/",
-    assets_img_dir = "assets/img/",
-    dev_dir = "dev/",
-    dev_js_dir = "dev/js/",
-    dev_layouts_dir = "dev/layouts/",
-    dev_pages_dir = "dev/pages/",
-    dev_scss_dir = "dev/scss/",
-    dev_scss_sub_dir = "dev/scss/*/",
-    dev_vendor_dir = "dev/vendor/";
-
-
-
-
-/* 
-    Gorev I : sass
-    
-    Ana sass dosyasini (dev/scss/main.scss) css'e cevir ve cevirilen css dosyasini minify edilip assets/css klasorune main.min.css olarak kaydedilmesi.
-    
-    */
-
-    gulp.task("sass", function() {
-        return gulp.src(dev_scss_dir + "main.scss")
-        .pipe(sass())
-        .pipe(minify_css({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename('main.min.css'))
-        .pipe(gulp.dest(assets_css_dir));
-    });
+var gulp = require("gulp"),
+sass = require("gulp-sass"),
+uglify_js = require("gulp-uglifyjs"),
+minify = require("gulp-minify"),
+file_include = require("gulp-file-include"),
+concat_css = require("gulp-concat-css"),
+concat = require("gulp-concat"),
+minify_css = require("gulp-minify-css"),
+rename = require("gulp-rename"),
+htmlmin = require('gulp-htmlmin');
 
 
 
 
 /* 
 
-    Gorev II : minify_js
+Assets (assets) klasorunde urun ortamina hazir sekilde yapilandirilmis klasorler ve dosyalar bulunur.
+Dev (dev) klasorunde projenin yapilandirilandigi klasorler ve dosyalar bulunur.
 
-    dev/js içinde bulunan tüm dosyaları alıp minify ederek assets/js içine kaydeder, uzantılarından önce ".min" ekler
+*/
 
-    */
+var public_dir = "public/",
+assets_dir = public_dir + "assets/",
+assets_css_dir = public_dir + "assets/css/",
+assets_js_dir = public_dir + "assets/js/",
+assets_img_dir = public_dir + "assets/img/",
+dev_dir = "dev/",
+dev_js_dir = "dev/js/",
+dev_layouts_dir = "dev/layouts/",
+dev_pages_dir = "dev/pages/",
+dev_scss_dir = "dev/scss/",
+dev_scss_sub_dir = "dev/scss/*/",
+dev_vendor_dir = "dev/vendor/";
 
 
 
-    gulp.task("minify_js", function() {
-        return gulp.src(dev_js_dir + "*.js")
-        .pipe(minify({
-            ext:{
-                src:false,
-                min:'.min.js'
-            },
-            noSource: "*"
-        }))
-        .pipe(gulp.dest(assets_js_dir));
-    });
+
+/* 
+Gorev I : sass
+
+Ana sass dosyasini (dev/scss/main.scss) css'e cevir ve cevirilen css dosyasini minify edilip public/assets/css klasorune main.min.css olarak kaydedilmesi.
+
+*/
+
+gulp.task("sass", function() {
+    return gulp.src(dev_scss_dir + "main.scss")
+    .pipe(sass())
+    .pipe(minify_css({
+        compatibility: 'ie8'
+    }))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest(assets_css_dir));
+});
 
 
 
 
 /* 
 
-    Gorev IV : plugins_js
-    
-    Kullanilan eklentilerde bulunan javascriptlerin (dev/vendor) birlestirilip assets/js klasorune plugins.min.js olarak kaydedilmesi.
-    
-    */
+Gorev II : minify_js
 
+dev/js içinde bulunan tüm dosyaları alıp minify ederek public/assets/js içine kaydeder, uzantılarından önce ".min" ekler
 
-    gulp.task("plugins_js", function() {
-        // burada .src içindeki dizi'nin (köşeli parantez) içine, birleştirilmesi istenen eklentilerin js dosyalarının yolları, virgüllerle ayrılarak verilmelidir
-        return gulp.src([])
-        .pipe(concat("plugins.min.js"))
-        .pipe(uglify_js())
-        .pipe(gulp.dest(assets_js_dir));
-    });
+*/
 
 
 
-
-/* 
-
-    Gorev V : plugins_css
-    
-    Kullanilan eklentilerde bulunan css'lerin (dev/vendor) birlestirilip assets/css klasorune plugins.min.css olarak kaydedilmesi.
-    
-    */
-
-    gulp.task("plugins_css", function() {
-        // burada .src içindeki dizi'nin (köşeli parantez) içine, birleştirilmesi istenen eklentilerin css dosyalarının yolları, virgüllerle ayrılarak verilmelidir
-        return gulp.src([])
-        .pipe(concat_css("plugins.min.css"))
-        .pipe(minify_css({
-            compatibility: 'ie8'
-        }))
-        .pipe(gulp.dest(assets_css_dir));
-    });
-
-
-
-
-
-/* 
-    Gorev VI : bootstrap
-    
-    Bootstrap kutuphanesinin ozellestirilmis sass dosyalarının css e cevirilip assets/css klasorune bootstap.min.css olarak kaydedilmesi.
-    
-    */
-
-    gulp.task("bootstrap", function() {
-        return gulp.src(dev_scss_dir + "bootstrap.scss")
-        .pipe(sass())
-        .pipe(minify_css({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename('bootstrap.min.css'))
-        .pipe(gulp.dest(assets_css_dir));
-    });
-
-
-
-
-
-/* 
-    Gorev VII : file_include
-    
-    Parcalanmis html sablonlarini birlestirilmesi ve ana dizine kaydedilmesi.
-    
-    */
-
-    gulp.task('file_include', function() {
-        return gulp.src(dev_pages_dir + "*.html")
-        .pipe(file_include({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
-        .pipe(gulp.dest("./"));
-    });
-
-
-/* 
-
-    Degisiklik yapildikca calisacak gorevler
-    
-    Gorev I (sass),
-    Gorev II (external_js),
-    Gorev III (main_js),
-    Gorev VI (bootstrap)
-    Gorev VII (file_include)
-    
-    */
-
-    gulp.task("watch", function() {
-        gulp.watch(dev_scss_sub_dir + "*.scss", ['sass']);
-        gulp.watch(dev_js_dir + "*.js", ['minify_js']);
-        gulp.watch(dev_scss_dir + "bootstrap/*.scss", ['bootstrap']);
-        gulp.watch(dev_pages_dir + "*.html", ['file_include']);
-        gulp.watch(dev_layouts_dir + "*.html", ['file_include']);
-        gulp.watch(dev_layouts_dir + "*/*.html", ['file_include']);
-    });
-
+gulp.task("minify_js", function() {
+    return gulp.src(dev_js_dir + "*.js")
+    .pipe(minify({
+        ext:{
+            src:false,
+            min:'.min.js'
+        },
+        noSource: "*"
+    }))
+    .pipe(gulp.dest(assets_js_dir));
+});
 
 
 
 
 /* 
 
-    Varsayilan olarak calistarilacak gorevler
-    
-    Gorev I (sass),
-    Gorev II (external_js),
-    Gorev III (main_js),
-    Gorev IV (plugins_js)
-    Gorev V (plugins_css)
-    Gorev VI (bootstrap)
-    Gorev VII (file_include)
-    
-    */
+Gorev IV : plugins_js
 
-    gulp.task("default", ["watch", "sass", "minify_js", "plugins_js", "plugins_css", "bootstrap", "file_include"]);
+Kullanilan eklentilerde bulunan javascriptlerin (dev/vendor) birlestirilip public/assets/js klasorune plugins.min.js olarak kaydedilmesi.
+
+*/
+
+
+gulp.task("plugins_js", function() {
+    /*return gulp.src()
+    .pipe(concat("plugins.min.js"))
+    .pipe(uglify_js())
+    .pipe(gulp.dest(assets_js_dir));
+    */
+});
+
+
+
+
+/* 
+
+Gorev V : plugins_css
+
+Kullanilan eklentilerde bulunan css'lerin (dev/vendor) birlestirilip public/assets/css klasorune plugins.min.css olarak kaydedilmesi.
+
+*/
+
+gulp.task("plugins_css", function() {
+    /*
+    return gulp.src()
+    .pipe(concat_css("plugins.min.css"))
+    .pipe(minify_css({
+        compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest(assets_css_dir));
+    */
+});
+
+
+
+
+
+/* 
+Gorev VI : bootstrap
+
+Bootstrap kutuphanesinin ozellestirilmis sass dosyalarının css e cevirilip public/assets/css klasorune bootstap.min.css olarak kaydedilmesi.
+
+*/
+
+gulp.task("bootstrap", function() {
+    return gulp.src(dev_scss_dir + "bootstrap.scss")
+    .pipe(sass())
+    .pipe(minify_css({
+        compatibility: 'ie8'
+    }))
+    .pipe(rename('bootstrap.min.css'))
+    .pipe(gulp.dest(assets_css_dir));
+});
+
+
+
+
+
+/* 
+Gorev VII : file_include
+
+Parcalanmis html sablonlarini birlestirilmesi ve /public dizinine kaydedilmesi.
+
+*/
+
+gulp.task('file_include', function() {
+    return gulp.src(dev_pages_dir + "*.html")
+    .pipe(file_include({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
+    .pipe(gulp.dest(public_dir));
+});
+
+
+/* 
+
+Degisiklik yapildikca calisacak gorevler
+
+Gorev I (sass),
+Gorev II (external_js),
+Gorev III (main_js),
+Gorev VI (bootstrap)
+Gorev VII (file_include)
+
+*/
+
+gulp.task("watch", function() {
+    gulp.watch(dev_scss_sub_dir + "*.scss", ['sass']);
+    gulp.watch(dev_js_dir + "*.js", ['minify_js']);
+    gulp.watch(dev_scss_dir + "bootstrap/*.scss", ['bootstrap']);
+    gulp.watch(dev_pages_dir + "*.html", ['file_include']);
+    gulp.watch(dev_layouts_dir + "*.html", ['file_include']);
+    gulp.watch(dev_layouts_dir + "*/*.html", ['file_include']);
+});
+
+
+
+
+
+/* 
+
+Varsayilan olarak calistarilacak gorevler
+
+Gorev I (sass),
+Gorev II (external_js),
+Gorev III (main_js),
+Gorev IV (plugins_js)
+Gorev V (plugins_css)
+Gorev VI (bootstrap)
+Gorev VII (file_include)
+
+*/
+
+gulp.task("default", ["watch", "sass", "minify_js", "plugins_js", "plugins_css", "bootstrap", "file_include"]);
